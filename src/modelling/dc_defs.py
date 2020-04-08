1463162fd1eeb4d4f5784442885276cbe22ad8f4
4g <apurvagup@gmail.com>
2020-04-08 18:27:21 +0530

    per part models

--- src/modelling/dc_defs.py
+++ src/modelling/dc_defs.py
@@ -26,5 +26,28 @@ chiller_input_params = ['CH_{n}_RETURN',
                         'CW_{n}_RETURN',
                         'CH_{n}/Evaporator Saturation Temp']
 
-dc_full_input = ['PDU/ZONE_2/DC_TOTAL_LOAD', 'oat1', 'oah', 'CH_{n}/Evaporator Saturation Temp']
-dc_full_output = ['PDU/ZONE_2/PUE']
+pue_full_input = ['PDU/ZONE_2/DC_TOTAL_LOAD', 'oat1', 'oah', 'CH_{n}/Evaporator Saturation Temp']
+pue_full_output = ['PDU/ZONE_2/PUE']
+
+pahu_input_params = ["PAHU_SA_SP", "PAHU_RA_SP", "PAHU RETURN_TEMP"]
+pahu_output_params = ["PAHU FAN_SPEED", "PAHU SUP_TEMP"]
+
+data_hall_input_params = ["POD TEMP-{n}", "POD HUM-{n}", "PAHU RA SP", "PAHU SA SP", "CH_{n}_SUPPLY", "IT LOAD"]
+data_hall_output_params = ["PUE", "PAHU-{n} TOTAL POWER"]
+
+
+param_dict = {
+    "pue_full" : [pue_full_input, pue_full_output],
+    "pahu" : [pahu_input_params, pahu_output_params],
+    "data_hall" : [data_hall_input_params, data_hall_output_params],
+    "chiller" : [chiller_input_params, chiller_output_params]
+}
+
+system_map = {
+    chiller.input: pahu.output1,
+    chiller.output: pahu.input1,
+
+    data_hall.input: pahu.output2,
+    data_hall.output: pahu.input2,
+}
+
--- src/modelling/part_model/part.py
+++ src/modelling/part_model/part.py
@@ -1,3 +1,6 @@
+from tensorflow import keras
+from tqdm import tqdm
+
 class System:
     def __init__(self):
         self.connection_graph = {}
@@ -18,12 +21,25 @@ class System:
 
 
 class Part:
-    def __init__(self, input_params, output_params):
+    def __init__(self, name, input_params, output_params):
         self.input_params = input_params
         self.output_params = output_params
-        self.model = None
+        self.model = Part.create_model(len(self.input_params), len(self.output_params))
 
-    def load_data(self, infile):
+    @staticmethod
+    def create_model(indim, outdim):
+        input_layer = keras.layers.Input(shape=(indim))
+        x = keras.layers.Dense(32, activation='relu')(input_layer)
+        x = keras.layers.Dense(32, activation='relu')(x)
+        x = keras.layers.Dense(32, activation='relu')(x)
+        x = keras.layers.Dense(32, activation='relu')(x)
+        output = keras.layers.Dense(outdim, activation='relu')(x)
+
+        model = keras.Model(inputs=input_layer, outputs=output)
+        model.compile(optimizer=keras.optimizers.SGD(lr=0.01), loss='mse', metrics=['mse', 'mae'])
+        return model
+
+    def load_data(self, indata, outdata):
         pass
 
     def train_model(self):
@@ -33,3 +49,33 @@ class Part:
         output = self.model.predict(input)
         output_dict = {}
         return output_dict
+
+
+class Data:
+    def __init__(self):
+        pass
+
+    def load_data(self):
+        pass
+
+    def partition(self):
+        pass
+
+    def preprocess(self):
+        pass
+
+def main():
+    param_tuples = load_part_params()
+    system = System()
+
+    for part_name, inparams, outparams in param_tuples:
+        desc = F"""Training part {part_name}"""
+        print (desc)
+        part = Part(part_name, inparams, outparams)
+        input_data = load_param_data(inparams)
+        output_data = load_param_data(outparams)
+        part.load_data(input_data, output_data)
+        part.train_model()
+        system.add_part(part)
+
+    system_map = load_system_part_map()
