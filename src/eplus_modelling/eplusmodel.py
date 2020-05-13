d994324a560506dfde6c61ee679dde48f7b10d3f
4g <apurvagup@gmail.com>
2020-05-13 09:58:02 +0530

    adding modifiable params to dashboard

--- src/eplus_modelling/eplusmodel.py
+++ src/eplus_modelling/eplusmodel.py
@@ -63,48 +63,52 @@ class EplusModelDC:
 
     def parse_schedule(self, schedule):
         data = schedule.to_dict()
-        prev = None
         for key, value in data.items():
             if isinstance(value, str) and self.isvalue(value):
-                self.schedule_index_map[(schedule.name, prev)] = key
-            prev = value
+                self.schedule_index_map[schedule.name] = self.schedule_index_map.get(schedule.name, [])
+                self.schedule_index_map[schedule.name].append(key)
 
-    def update_schedule(self, name, key, value):
-        schedule_key = self.schedule_index_map[(name, key)]
-        self.schedules[name][schedule_key] = value
+    def update_schedule(self, name, value):
+        schedule_keys = self.schedule_index_map[name]
+        for key in schedule_keys:
+            self.schedules[name][key] = value
 
     def simulate(self, odir):
-        op.simulate(self.epm, self.weather, odir)
+        return op.simulate(self.epm, self.weather, odir)
 
     def get_modifiables(self):
-        return self.schedule_index_map
+        return list(self.schedule_index_map.keys())
 
 class EplusExperiment:
     def __init__(self, name):
-        self.eplusmodel_a = EplusModelDC("dc_specs/2ZoneDataCenterHVAC_wEconomizer.idf", "dc_specs/IND_Bangalore.432950_ISHRAE.epw")
-        self.eplusmodel_b = EplusModelDC("dc_specs/2ZoneDataCenterHVAC_wEconomizer.idf", "dc_specs/IND_Bangalore.432950_ISHRAE.epw")
+        self.eplusmodel_a = EplusModelDC("eplus_modelling/dc_specs/2ZoneDataCenterHVAC_wEconomizer.idf", "eplus_modelling/dc_specs/IND_Bangalore.432950_ISHRAE.epw")
+        self.eplusmodel_b = EplusModelDC("eplus_modelling/dc_specs/2ZoneDataCenterHVAC_wEconomizer.idf", "eplus_modelling/dc_specs/IND_Bangalore.432950_ISHRAE.epw")
         self.name = name
 
-    def set_ab(self, name, key, a, b):
-        self.eplusmodel_a.update_schedule(name, key, a)
-        self.eplusmodel_b.update_schedule(name, key, b)
+    def set_ab(self, name, a, b):
+        self.eplusmodel_a.update_schedule(name, a)
+        self.eplusmodel_b.update_schedule(name, b)
 
     def run(self):
-        self.eplusmodel_a.simulate(self.name + "_a")
-        self.eplusmodel_b.simulate(self.name + "_b")
-    
+        sim_a = self.eplusmodel_a.simulate(self.name + "_a")
+        sim_b = self.eplusmodel_b.simulate(self.name + "_b")
+        a_eso = sim_a.get_out_eso().get_data()
+        b_eso = sim_b.get_out_eso().get_data()
+        return a_eso, b_eso
+
     def set_period(self, start, end):
         self.eplusmodel_a.set_period(start, end)
         self.eplusmodel_b.set_period(start, end)
 
+    def get_modifiables(self):
+        return self.eplusmodel_a.get_modifiables()
+
 if __name__ == "__main__":
     eplusmodel = EplusExperiment("heating_cooling_setpoints")
     eplusmodel.set_period(start=(1, 6), end=(2, 6))
-    eplusmodel.set_ab("heating setpoints", "until: 7:00", str(15), str(20))
-    eplusmodel.set_ab("heating setpoints", "until: 17:00", str(15), str(20))
-    eplusmodel.set_ab("heating setpoints", "until: 24:00", str(15), str(20))
-
-    eplusmodel.set_ab("cooling setpoints", "until: 7:00", str(18), str(35))
-    eplusmodel.set_ab("cooling setpoints", "until: 17:00", str(18), str(35))
-    eplusmodel.set_ab("cooling setpoints", "until: 24:00", str(18), str(35))
-    eplusmodel.run()
+    eplusmodel.set_ab("heating setpoints", str(15), str(20))
+    eplusmodel.set_ab("cooling setpoints",  str(18), str(35))
+    a,b = eplusmodel.run()
+    print(eplusmodel.eplusmodel_a.get_modifiables())
+    print (a)
+    print (b)
--- src/ui/runserver.py
+++ src/ui/runserver.py
@@ -2,11 +2,13 @@ from flask import Flask
 from gevent.pywsgi import WSGIServer
 from random import randint
 import pandas as pd
-from flask import make_response
+from flask import make_response, redirect, url_for
 import json
 import gzip
+from eplus_modelling.eplusmodel import EplusExperiment
 
 app = Flask(__name__)
+experiments = {}
 
 class DFTimeSeriesGen:
     def __init__(self, df_csv_path):
@@ -41,7 +43,6 @@ class DFTimeSeriesGen:
         # return list(filter(lambda x: "_sp" in x.lower()[-4:], self.columns))
         return list(self.columns)
 
-
 class TimeSeriesGen:
     def __init__(self, num_cols):
         self.index = 0
@@ -60,34 +61,46 @@ class TimeSeriesGen:
         self.value = randint(18, 25)
         return [randint(0, 15) for col in self.cols]
 
-class ABTimeSeries:
-    def __init__(self, df_csv_path):
-        self.experiment = self.create_experiment(a, b)
+class ABExperimentTimeSeries:
+    def __init__(self):
+        self.eplus_exp = EplusExperiment("experiment_server")
+        self.results_a, self.results_b = None, None
         self.index = 0
-        self.columns = self.data.columns.tolist()
+        self.columns = []
 
-    def next(self):
-        row = self.data.iloc[self.index]
-        self.index = (self.index + 360) % len(self.data)
-        return list(row.values)
-
-    def row_as_dict(self, row):
-        return dict(zip(self.columns, row))
-
-    def header(self):
-        # header = list(filter(lambda x: "ems" in x.lower() and ("pdu" not in x.lower()), self.columns))
-        header = list(self.columns)
-        return header
+    def run_experiment(self, setpoints_a, setpoints_b):
+        self.eplus_exp.set_period(start=(1, 12), end=(5, 12))
+        self.eplus_exp.set_ab("heating setpoints", str(15), str(20))
+        self.eplus_exp.set_ab("cooling setpoints", str(18), str(35))
+        self.results_a, self.results_b = self.eplus_exp.run()
+        self.columns = self.results_b.columns.to_list()
+        self.columns = list(filter(lambda x: "power" in x.lower(), self.columns))
 
     def modifiable(self):
-        # return list(filter(lambda x: "_sp" in x.lower()[-4:], self.columns))
-        return list(self.columns)
+        return self.eplus_exp.get_modifiables()
 
-# tsgen = TimeSeriesGen(9)
+    def header(self):
+        return self.columns
+
+    def next(self):
+        # print (self.index, self.results_b)
+        row_a = self.results_a[self.columns].iloc[self.index]
+        row_b = self.results_b[self.columns].iloc[self.index]
+        self.index = (self.index + 1)
+        tofloat = lambda l : list(map(float, list(l)))
+        return [tofloat(row_a.values), tofloat(row_b.values)]
+
+tsgen = TimeSeriesGen(9)
 # tsgen = DFTimeSeriesGen("../outputs/flipkart/flat_df.csv")
 # tsgen = DFTimeSeriesGen("../data/modelling/eplusout.csv")
-tsgen = DFTimeSeriesGen("../src/eplus_modelling/usual/eplusout.csv")
+# tsgen = DFTimeSeriesGen("../eplus_modelling/usual/eplusout.csv")
 
+# tsgen = ABExperimentTimeSeries()
+
+@app.route('/experiment')
+def run_experiment(setpoints=None):
+    tsgen.run_experiment(1, 2)
+    return {'status':'finished'}
 
 @app.route('/')
 def blank():
