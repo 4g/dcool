410c48a5b101962c4319c99d533f4e22f56a4711
4g <apurvagup@gmail.com>
2020-06-03 11:07:06 +0530

    adding basic setup script for setup and deleting empty file

--- src/setup.sh
+++ src/setup.sh
@@ -1,24 +1,10 @@
-# Add NVIDIA package repositories
-wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/cuda-repo-ubuntu1804_10.1.243-1_amd64.deb
-sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/7fa2af80.pub
-sudo dpkg -i cuda-repo-ubuntu1804_10.1.243-1_amd64.deb
-sudo apt-get update
-wget http://developer.download.nvidia.com/compute/machine-learning/repos/ubuntu1804/x86_64/nvidia-machine-learning-repo-ubuntu1804_1.0.0-1_amd64.deb
-sudo apt install ./nvidia-machine-learning-repo-ubuntu1804_1.0.0-1_amd64.deb
-sudo apt-get update
-
-# Install NVIDIA driver
-sudo apt-get install --no-install-recommends nvidia-driver-430
-# Reboot. Check that GPUs are visible using the command: nvidia-smi
-
-# Install development and runtime libraries (~4GB)
-sudo apt-get install --no-install-recommends \
-    cuda-10-1 \
-    libcudnn7=7.6.4.38-1+cuda10.1  \
-    libcudnn7-dev=7.6.4.38-1+cuda10.1
-
-
-# Install TensorRT. Requires that libcudnn7 is installed above.
-sudo apt-get install -y --no-install-recommends libnvinfer6=6.0.1-1+cuda10.1 \
-    libnvinfer-dev=6.0.1-1+cuda10.1 \
-    libnvinfer-plugin6=6.0.1-1+cuda10.1
+#!/bin/bash
+python3 -m venv --system-site-packages ~/py3env2
+source ~/py3env2/bin/activate
+pip install tensorflow
+pip install gym
+pip install keras-rl
+pip install keras
+pip install pygame
+pip install pymunk
+pip install matplotlib
